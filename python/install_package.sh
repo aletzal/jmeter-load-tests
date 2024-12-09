bd2ec23261239009d609a151681cec660bc83ad4
Alexey Zakharov <alexey.zakharov@1win.pro>
2024-12-10 01:31:22 +0300

    add some logs

--- python/install_package.sh
+++ python/install_package.sh
@@ -2,25 +2,21 @@
 
 set -e
 
-# Parameters passed to the script
 THREAD_NUM=$1
 REPO_URL=$2
 USERNAME=$3
 PASSWORD=$4
 
-# Target directory for the package installation
 TARGET_DIR="./tmp/testfolder_${THREAD_NUM}"
 
-# Remove old target directory
+echo "Recreate target folder"
 rm -rf ${TARGET_DIR}
-
-# Create the target directory if it doesn't exist
 mkdir -p ${TARGET_DIR}
 
-# Run pip install with the custom repository and authentication
+echo "Install packages by requirements.txt from the kodera repository"
 pip install -r requirements.txt --disable-pip-version-check --index-url https://${USERNAME}:${PASSWORD}@${REPO_URL}/simple ${PACKAGE_NAME} --trusted-host localhost --no-cache-dir --target ${TARGET_DIR}
 
-echo "Cleanuo"
-rm -rf ${TARGET_DIR}
-
 echo "Kodera - successfully installed packages"
+
+echo "Cleanup"
+rm -rf ${TARGET_DIR}
--- python/upload/upload_to_custom_repo.sh
+++ python/upload/upload_to_custom_repo.sh
@@ -2,20 +2,16 @@
 
 set -e
 
-# Install dependencies (if they are not installed already)
-echo "Installing required dependencies..."
-pip install --upgrade pip setuptools wheel twine
-
-# Remove any existing .pypirc file to ensure it is overwritten
-echo "Removing existing .pypirc file (if any)..."
-rm -rf ./.pypirc
-
-# Define repository details for .pypirc file
 REPO_URL=$1
 REPO_USERNAME=$2
 REPO_PASSWORD=$3
 
-# Create .pypirc file dynamically
+echo "Installing required dependencies..."
+pip install --upgrade pip setuptools wheel twine
+
+echo "Removing existing .pypirc file (if any)..."
+rm -rf ./.pypirc
+
 echo "Creating .pypirc file..."
 cat <<EOF > ./.pypirc
 [distutils]
@@ -28,14 +24,11 @@ username = ${REPO_USERNAME}
 password = ${REPO_PASSWORD}
 EOF
 
-# Create a directory for downloaded packages
 DOWNLOAD_DIR="dist"
-echo "Delete downloaded packages folder"
+echo "Recreate downloaded packages folder"
 rm -rf ${DOWNLOAD_DIR}
-echo "Creating folder for downloaded packages"
 mkdir -p ${DOWNLOAD_DIR}
 
-# Download the specified packages from requirements.txt to the created directory
 echo "Downloading packages from requirements.txt..."
 if [[ -f "../requirements.txt" ]]; then
     pip download -r ../requirements.txt -d ${DOWNLOAD_DIR}
@@ -45,12 +38,11 @@ else
     exit 1
 fi
 
-# Upload downloaded packages to the custom repository
-echo "Uploading packages to custom repository..."
+echo "Uploading packages to kodera repository..."
 python3 -m twine upload --verbose --cert localhost.crt --repository kodera --config-file ./.pypirc ${DOWNLOAD_DIR}/*
+
+echo "Kodera - successfully uploaded packages"
 
 echo "Cleanup"
 rm -rf ./.pypirc
 rm -rf ${DOWNLOAD_DIR}
-
-echo "Kodera - successfully uploaded packages"
